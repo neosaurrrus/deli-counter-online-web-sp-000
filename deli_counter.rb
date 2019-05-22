bec21dfe7b49a20b333c07e52bf4840636c92549
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 22:31:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size < 1
     puts "The line is currently empty."
+    return
   end
   serving = []
   katz_deli.each_with_index do | customer, index |
