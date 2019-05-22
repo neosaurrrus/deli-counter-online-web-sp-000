8259e07297e2939c3d3691002edc9377ea3f5cae
Lukie Kang <lukiek+f@gmail.com>
2019-05-22 21:59:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,11 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.size == 0
-    return "The line is currently empty"
+    return "The line is currently empty."
   end
 end
+
+def take_a_number(katz_deli, customer)
+  katz_deli << customer
+  puts "Welcome #{customer}. You are #{katz_deli.size} in line."
+  
